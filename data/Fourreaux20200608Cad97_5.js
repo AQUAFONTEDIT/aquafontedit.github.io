2a4a202f7a548c8a26049dd0c28d9ca44058fdf1
AQUA FONTEDIT <63478683+AQUAFONTEDIT@users.noreply.github.com>
2020-06-29 08:57:02 +0200

    Add files via upload

--- data/Fourreaux20200608Cad97_5.js
+++ data/Fourreaux20200608Cad97_5.js
@@ -3,9 +3,10 @@ var json_Fourreaux20200608Cad97_5 = {
 "name": "Fourreaux20200608Cad97_5",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "qc_id": 1.0, "name": "F55", "map_name": "Trac�_def_AVP_08-06-2020.kmz - Fourreau_en_attente_pour_reseau_privé", "kml_style": "msn_placemark_circle10", "kml_folder": "Fourreau_en_attente_pour_reseau_privé" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.15592801864782, 43.45311946830671 ] ] } },
 { "type": "Feature", "properties": { "qc_id": 2.0, "name": "F61", "map_name": "Trac�_def_AVP_08-06-2020.kmz - Fourreau_en_attente_pour_reseau_privé", "kml_style": "msn_F", "kml_folder": "Fourreau_en_attente_pour_reseau_privé" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.156399690682205, 43.458674907922791 ] ] } },
 { "type": "Feature", "properties": { "qc_id": 3.0, "name": "F63", "map_name": "Trac�_def_AVP_08-06-2020.kmz - Fourreau_en_attente_pour_reseau_privé", "kml_style": "msn_F", "kml_folder": "Fourreau_en_attente_pour_reseau_privé" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.15725169742636, 43.463068174639417 ] ] } },
-{ "type": "Feature", "properties": { "qc_id": 4.0, "name": "F43", "map_name": "Trac�_def_AVP_08-06-2020.kmz - Fourreau_en_attente_pour_reseau_privé", "kml_style": "msn_F2", "kml_folder": "Fourreau_en_attente_pour_reseau_privé" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.169233854471466, 43.459512108784033 ] ] } }
+{ "type": "Feature", "properties": { "qc_id": 4.0, "name": "F43", "map_name": "Trac�_def_AVP_08-06-2020.kmz - Fourreau_en_attente_pour_reseau_privé", "kml_style": "msn_F2", "kml_folder": "Fourreau_en_attente_pour_reseau_privé" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.169233854471466, 43.459512108784033 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 5.0, "name": "F46", "map_name": "Trac�_def_AVP_08-06-2020.kmz - Fourreau_en_attente_pour_reseau_privé", "kml_style": "msn_F2", "kml_folder": "Fourreau_en_attente_pour_reseau_privé" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.169254152009524, 43.466788107619372 ] ] } },
+{ "type": "Feature", "properties": { "qc_id": 8.0, "name": "F32", "map_name": "Trac�_def_AVP_08-06-2020.kmz - Fourreau_en_attente_pour_reseau_privé", "kml_style": "msn_F3", "kml_folder": "Fourreau_en_attente_pour_reseau_privé" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 3.183574313104006, 43.459598046596398 ] ] } }
 ]
 }
